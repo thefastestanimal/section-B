97d181813b859927ed6d8b7c7f750c015b7e49e3
OMG <165387453+thefastestanimal@users.noreply.github.com>
2025-03-28 16:37:58 +0300

    Create lower.py

---
+++ 1107_15/string_method/lower/lower.py
@@ -0,0 +1,4 @@
+print("HELLO".lower())    # Output: "hello"
+print("Hello World".lower())  # Output: "hello world"
+print("123ABC".lower())   # Output: "123abc"
+print("python".lower())   # Output: "python" (No change as it's already lowercase)
