5f628f3bebeeb7940204437108978b1bfa38abca
OMG <165387453+thefastestanimal@users.noreply.github.com>
2025-03-28 16:30:31 +0300

    Create islower.py

---
+++ 1107_15/string_method/islower/islower.py
@@ -0,0 +1,4 @@
+print("hello".islower())   # Output: True
+print("Hello".islower())   # Output: False
+print("123".islower())     # Output: False (No alphabetic characters)
+print("hello123".islower()) # Output: True (Non-alphabetic characters are ignored)
