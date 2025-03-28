0de5bb015c69423599eeb50906b58a321e1f0b49
OMG <165387453+thefastestanimal@users.noreply.github.com>
2025-03-28 16:19:25 +0300

    Create capitalize.py

---
+++ 1107_15/string_method/capitalize/capitalize.py
@@ -0,0 +1,4 @@
+print("hello world".capitalize())  # Output: "Hello world"
+print("PYTHON".capitalize())       # Output: "Python"
+print("123abc".capitalize())       # Output: "123abc" (Numbers remain unchanged)
+print("hElLo".capitalize())        # Output: "Hello"
