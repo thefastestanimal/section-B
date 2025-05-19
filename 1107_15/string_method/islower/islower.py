cca01ce8e63a23cd39462d219e9076c80bb3c2f1
OMG <165387453+thefastestanimal@users.noreply.github.com>
2025-05-19 15:47:33 +0300

    Delete 1107_15/string_method/islower/islower.py

--- 1107_15/string_method/islower/islower.py
+++
@@ -1,4 +0,0 @@
-print("hello".islower())   # Output: True
-print("Hello".islower())   # Output: False
-print("123".islower())     # Output: False (No alphabetic characters)
-print("hello123".islower()) # Output: True (Non-alphabetic characters are ignored)
